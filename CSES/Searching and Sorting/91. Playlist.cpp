24d4b3f2ebdc5159aebd717dc2afe0b854737bd2
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-16 14:09:13 +0530

    Update 91. Playlist.cpp

--- CSES/Searching and Sorting/91. Playlist.cpp
+++ CSES/Searching and Sorting/91. Playlist.cpp
@@ -1,1 +1,26 @@
+void solve()
+{
+    int i, j, n;
+    cin >> n;
+    vector<int> k(n);
+    fo(i, n) cin >> k[i];
 
+    set<int> set;
+    int ans = 0;
+    i = 0, j = 0;
+    while (i < n && j < n)
+    {
+        while (j < n && !set.count(k[j]))
+        {
+            set.insert(k[j]);
+            ans = max(ans, j - i + 1);
+            j++;
+        }
+        while (j < n && set.count(k[j]))
+        {
+            set.erase(k[i]);
+            i++;
+        }
+    }
+    cout << ans << nl;
+}
