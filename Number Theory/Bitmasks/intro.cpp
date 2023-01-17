a1e0b1f4eb9e64ab25bce148f3c78cae72be98c1
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-17 08:47:24 +0530

    Create intro.cpp

---
+++ Number Theory/Bitmasks/intro.cpp
@@ -0,0 +1,18 @@
+void solve()
+{
+    // using bits to store info
+    // Bitmasks
+    // intersection in O(1) --> take &
+    // eg. 1st and 3rd bit is set
+    // max unsigned int(32) or ull(64) --> The limitation otherwise why would you use sets
+    // USE WHEN NUMBER IS LESS LIKE < 32 || < 64
+
+    // eg Q
+    // we have workers who work on different days find the max common working days b/w any 2
+    // https://codeforces.com/blog/entry/73558
+
+    // to set a bit --> mask = {0, 0, 0}
+    // mask = (mask | (1 << days)) --> sets the dayth bit
+
+    // __builtin_popcountll to find the number of set bits
+}
