83bf8b23455eb281b1d94b5cd4a98922bdde2e15
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-27 00:47:08 +0530

    Create 1. Modular Multiplicative Inverse.cpp

---
+++ Number Theory/Number Theory/1. Modular Multiplicative Inverse.cpp
@@ -0,0 +1,26 @@
+// (a/b) % M == (a * b^-1) % M
+//             == ((a%M) * (b^-1 % M)) % M
+
+// b^-1 % M --> Modular Multiplicative inverse of b
+
+/* 
+  a*b == 1
+  then b is multiplicative inverse of a
+  
+  similarly, (a*b)%M == 1
+  b is MMI of a
+  1 <= b <= M-1
+  (a%M * b%M)%M == 1
+  loop b = 1 to M-1 to find MMI of a --> O(1)
+  
+  MMI is defined only if a and b are coprime
+  
+  FERMAT'S THEOREM
+  Congruency of Numbers --> A^(M-1) % M = 1 where M is prime and A is not a multiple of M
+  mulitply both sides by A^-1
+  (A^(M - 2)) % M == A^-1
+  
+  // to declare a global array
+  const int N = 1e5 + 1;
+  int globalArray[N];
+*/
