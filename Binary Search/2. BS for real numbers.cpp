d6230d80d7368a8b8c5191ad8d2892fdc9e893d3
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-02-05 13:24:17 +0530

    Create 2. BS for real numbers.cpp

---
+++ Binary Search/2. BS for real numbers.cpp
@@ -0,0 +1,13 @@
+/* 
+  for real numbers
+  1. epsilon = 0.0000001
+  while(r - l >= epsilon)
+  
+  widely used method
+  2. pick a random number b/2 50 60
+  and do those many iteations of binary searchh
+  
+  // Ropes
+  // https://codeforces.com/edu/course/2/lesson/6/2/practice/contest/283932/problem/B
+  
+*/
