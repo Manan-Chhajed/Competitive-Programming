474a12982621c4f36c339c3a810e14645a3cdbdf
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-02-11 21:21:27 +0530

    Create 1. intro.cpp

---
+++ DP/Kartik Arora/1. intro.cpp
@@ -0,0 +1,15 @@
+// DP IS VERY EASY. TRUST ME BRO!
+
+/* 
+  1. optimizaton problems
+  2. counting problems
+  
+  DP STATES
+  
+  DAG REPRESENTATION
+    nodes --> dp states
+    edges --> dependencies
+    
+  TC --> no. of states * transition time
+  SC --> no. of states
+*/
