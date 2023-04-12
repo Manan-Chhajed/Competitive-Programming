ac41b5af79710bea48827402b91c858e52e8a0e8
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-04-13 01:14:36 +0530

    Update 5. Prime factorization using sieve in logn.cpp

--- Number Theory/Number Theory/5. Prime factorization using sieve in logn.cpp
+++ Number Theory/Number Theory/5. Prime factorization using sieve in logn.cpp
@@ -1,3 +1,19 @@
 
-
-
+void primeFact(int n)
+{
+  for(int i=2; i*i<=n; i++)
+  {
+    if(n % i == 0)
+    {
+      int cnt = 0;
+      while(n % i == 0)
+      {
+        cnt++;
+        n /= i;
+      }
+      cout << i << " " << cnt << endl;
+    }
+    if(n > 1)
+      cout << n << " " << 1 << endl;
+  }
+}
