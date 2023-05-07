24c25e7eee97b3cd209b14b0ced810759e00c447
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-05-07 22:42:06 +0530

    Create 2. Lazy Propagation.cpp

---
+++ Segment Tree/TLE/2. Lazy Propagation.cpp
@@ -0,0 +1,11 @@
+/* 
+  Normal segment tree
+  1. Point updates
+  2. Range queries
+
+  Now, what if?
+  1. Update all the values from l to r            O(log n)
+  2. Range sum                                    O(log n)
+
+  
+*/
