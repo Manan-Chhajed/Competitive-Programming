7c1d62f46ea3ffc0c2d70300a37d1e6edafc6cef
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-06-19 19:58:09 +0530

    Update 4.dp_on_trees.cpp

--- TLE/Trees/4.dp_on_trees.cpp
+++ TLE/Trees/4.dp_on_trees.cpp
@@ -1,3 +1,10 @@
 /* 
-  
+  - Rerooting is a cool trick
+  - Some cool problems
+  https://codeforces.com/problemset/problem/191/C
+  https://codeforces.com/contest/1294/problem/F
+  https://codeforces.com/contest/1324/problem/F
+  https://codeforces.com/problemset/problem/1092/F
+  https://codeforces.com/problemset/problem/685/B
+  https://codeforces.com/problemset/problem/1746/D
 */
