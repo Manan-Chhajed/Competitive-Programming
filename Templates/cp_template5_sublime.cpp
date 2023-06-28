b66d144db9841bf8cb12bd46efee727acb351a5c
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-06-28 17:47:36 +0530

    Create cp_template5_sublime.cpp

---
+++ Templates/cp_template5_sublime.cpp
@@ -0,0 +1,30 @@
+/* Manan Chhajed */
+#include<bits/stdc++.h>
+using namespace std;
+
+void cmc() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("err.txt", "w", stderr);
+#endif
+}
+
+using ll = long long;
+
+void solve() {
+
+}
+
+int main() {
+    cmc();
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;
+}
