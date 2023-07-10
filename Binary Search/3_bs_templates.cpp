8c0c648c3e54ad50495e07899d543603acf45359
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-07-10 15:01:35 +0530

    Update 3_bs_templates.cpp

--- Binary Search/3_bs_templates.cpp
+++ Binary Search/3_bs_templates.cpp
@@ -10,3 +10,21 @@ if (l <= r)
 
     return binarySearch(arr, mid + 1, r, x);
 }
+
+while (l <= r)
+{
+    ll mid = (l + r) >> 1;
+    memset(dp, -1, sizeof dp);
+
+    string lim = to_string(mid);
+    ll nums = f(0, lim, 1) - 1; // find the nth number in a sequence
+    if (nums >= n)
+    {
+        r = mid - 1;
+        res = mid;
+    }
+    else
+    {
+        l = mid + 1;
+    }
+}
