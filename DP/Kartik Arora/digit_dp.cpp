a1c88b45d46fa0ff88169a9bbbad467fde9f52cf
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-07-10 13:09:23 +0530

    Update digit_dp.cpp

--- DP/Kartik Arora/digit_dp.cpp
+++ DP/Kartik Arora/digit_dp.cpp
@@ -1,1 +1,7 @@
-
+/* 
+  - https://leetcode.com/problems/count-of-integers/description/
+  - Just fancy name for dp
+  - Pattern 1 
+    - Count of integers x from 0 to r such that x obeys f(x)
+    - 
+*/
