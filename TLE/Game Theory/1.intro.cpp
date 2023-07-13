bd382166f2ace7569cbadf6e92955873abda73c2
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-07-14 01:08:10 +0530

    Update 1.intro.cpp

--- TLE/Game Theory/1.intro.cpp
+++ TLE/Game Theory/1.intro.cpp
@@ -1,6 +1,14 @@
 /* 
+  - Nim game 
+    - N piles of stones, remove any no. of stones from any pile
+    - If xor of all the piles is 0, other player wins, else current player wins
+
   - Nim Game Variations
     - Bogus Nim
     - Misere Nim
     - Staircase Nim
+    
+  - Sprague grundy theorem
+  - Grundy numbers
+  
 */
