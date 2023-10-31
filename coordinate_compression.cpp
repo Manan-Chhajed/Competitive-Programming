0702052baff999960d321c7969f76f26545c2b28
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-10-31 13:11:32 +0530

    Create coordinate_compression.cpp

---
+++ coordinate_compression.cpp
@@ -0,0 +1,9 @@
+ // coordinate compression
+vector<ll> d = a;
+sort(d.begin(), d.end());
+d.resize(unique(d.begin(), d.end()) - d.begin());
+for (ll i = 0; i < n; ++i)
+{
+    a[i] = lower_bound(d.begin(), d.end(), a[i]) - d.begin();
+}
+//original value of a[i] can be obtained through d[a[i]]
