253d7e6b69229c56fb8b878d49a37869a268e55f
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-12-27 18:12:46 +0530

    Update sparse_table.cpp

--- Codes/sparse_table.cpp
+++ Codes/sparse_table.cpp
@@ -7,6 +7,7 @@
     - then you can find for 1 to 4 and any range such that you reach end of query array (i.e. reach 7)
     - so you can choose 1 to 4 and 3 to 7, since idempotent, overlap of the segment is fine
     - Idempotent means say f(x, x) = x, e.g. min, max, gcd, bitwise and, bitwise or
-    - TC --> O(2) i.e. O(1) for 1 query
+    - TC --> O(1 * op) for 1 query
+    - Here op is the time for that particular query, say op for min is O(1) but for gcd is O(a) where a is larger of the 2 numbers
     - But still preprocessing of O(nlogn)
 */
