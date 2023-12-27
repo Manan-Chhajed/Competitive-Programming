438c90fc9510aae6f4ded0b6275a83af591ddb47
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-12-27 17:16:29 +0530

    Update sparse_table.cpp

--- Codes/sparse_table.cpp
+++ Codes/sparse_table.cpp
@@ -1,7 +1,10 @@
 /* 
   - Prefix sum doesn't work for non invertible operations
   - Sparse table works for invertible operations as well
-    - Invertible --> you know [1, 10] and [1, 15], can you find [10, 15]
-
-  
+    - Invertible means say you know answer for [1, 10] and [1, 15], can you find [10, 15]
+  - What if operation is idempotent?
+    - say you want answer for query 1 to 7, you can calculate for say 1 to 4(no. smaller than 7 that is a multiple of 2)
+    - then you can find for 1 to 4 and any range such that you reach end of query array (i.e. reach 7)
+    - so you can choose 1 to 4 and 3 to 7, since idempotent, overlap of the segment is fine
+    - Idempotent means say f(x, x) = x, e.g. min, max, gcd, bitwise and, bitwise or
 */
