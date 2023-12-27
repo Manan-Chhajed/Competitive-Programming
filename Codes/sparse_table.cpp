149bd598485eaf155c72d9c0e1c7adc778d8d36e
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-12-27 17:01:43 +0530

    Create sparse_table.cpp

---
+++ Codes/sparse_table.cpp
@@ -0,0 +1,7 @@
+/* 
+  - Prefix sum doesn't work for non invertible operations
+  - Sparse table works for invertible operations as well
+    - Invertible --> you know [1, 10] and [1, 15], can you find [10, 15]
+
+  
+*/
