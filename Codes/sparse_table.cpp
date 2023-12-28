9e4629ff26aa97404443700f4180cc751e4c1f57
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-12-28 18:41:35 +0530

    Update sparse_table.cpp

--- Codes/sparse_table.cpp
+++ Codes/sparse_table.cpp
@@ -11,3 +11,98 @@
     - Here op is the time for that particular query, say op for min is O(1) but for gcd is O(a) where a is larger of the 2 numbers
     - But still preprocessing of O(nlogn)
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void io() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("err.txt", "w", stderr);
+#endif
+}
+
+using ll = long long;
+
+const ll K = 22;
+const ll N = 1e5 + 1;
+ll st[K + 1][N];
+ll lg[N + 1];
+
+void sparsePre(vector<ll>& a)
+{
+    lg[1] = 0;
+    for (ll i = 2; i <= N; i++)
+    {
+        lg[i] = lg[i / 2] + 1;
+    }
+
+    copy(a.begin(), a.end(), st[0]);
+
+    // O(nlogn) preprocessing
+    for (ll i = 1; i <= K; i++)
+    {
+        for (ll j = 0; j + (1 << i) <= N; j++)
+        {
+            st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+        }
+    }
+}
+
+ll query(ll l, ll r)
+{
+    // range min query
+    ll i = lg[r - l + 1];
+    ll mini = min(st[i][l], st[i][r - (1 << i) + 1]);
+    return mini;
+
+    // range sum query
+    // ll sum = 0;
+    // for (int i = K; i >= 0; i--)
+    // {
+    //     if ((1 << i) <= r - l + 1)
+    //     {
+    //         sum += st[i][l];
+    //         l += 1 << i;
+    //     }
+    // }
+}
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+
+    sparsePre(a);
+
+    ll q;
+    cin >> q;
+    for (ll i = 0; i < q; i++)
+    {
+        ll l, r;
+        cin >> l >> r;
+
+        cout << query(l, r) << endl;
+    }
+}
+
+int main()
+{
+    io();
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        cerr << t << endl;
+        solve();
+        cerr << endl;
+    }
+
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;
+}
