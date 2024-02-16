5e8c8fea1369d2e63184501ece08f2e84f3332be
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2024-02-16 18:56:22 +0530

    Update Notes.cpp

--- Notes.cpp
+++ Notes.cpp
@@ -2,3 +2,9 @@
   - Meet in the Middle algo
   - https://leetcode.com/problems/partition-array-into-two-arrays-to-minimize-sum-difference/description/
 */
+
+/* 
+  Chicken McNugget Theorem
+  https://artofproblemsolving.com/wiki/index.php/Chicken_McNugget_Theorem
+  Basically, all multiples of gcd(m, n) greater than lcm(m, n) - m - n are reperesentable in the from am + bn for a > 0, b > 0.
+*/
