60e7cb72371e45aa1db275876fbfc1c3c4bd8942
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2024-02-16 18:57:38 +0530

    Delete Notes/Codefroces practice/1. Learnings.cpp

--- Notes/Codefroces practice/1. Learnings.cpp
+++
@@ -1,5 +0,0 @@
-// https://codeforces.com/contest/1573/problem/B
-// suffix array was the learning
-
-// difference array is a saver. 
-// A difference array can be used to perform multiple range update where we need to find the answer only after performing all the queries
