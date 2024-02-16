ff3eeba185e2f8d38ca9b7cd1b51a5027a47cf97
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2024-02-16 18:56:33 +0530

    Delete Notes/General.cpp

--- Notes/General.cpp
+++
@@ -1,5 +0,0 @@
-/* 
-  Chicken McNugget Theorem
-  https://artofproblemsolving.com/wiki/index.php/Chicken_McNugget_Theorem
-  Basically, all multiples of gcd(m, n) greater than lcm(m, n) - m - n are reperesentable in the from am + bn for a > 0, b > 0.
-*/
