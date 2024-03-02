5e1cf80435d2aeb7f13fab679b574a3921ce5cb7
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2024-03-02 13:38:34 +0530

    Create Notes_graph.cpp

---
+++ Notes_graph.cpp
@@ -0,0 +1,8 @@
+/* 
+  - Topological Sort for DAG i.e. directed acyclic sort
+
+  - DFS approach --> https://cp-algorithms.com/graph/topological-sort.html
+  - BFS approach --> Kahn's algo https://leetcode.com/problems/course-schedule-ii/submissions/1191319333
+    - Cycle detection 
+      - If topo sort has n vertices then no cycle, else cycle detected  
+*/
