4731cc99ca6ea1891eb3945e16453784f673f9a5
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2024-03-16 22:09:25 +0530

    Update lca_using_binary_lifting.cpp

--- Codes/lca_using_binary_lifting.cpp
+++ Codes/lca_using_binary_lifting.cpp
@@ -1,18 +1,18 @@
-int n, l;
-vector<vector<int>> adj;
+ll n, l;
+vector<vector<ll>> adj;
 
-int timer;
-vector<int> tin, tout;
-vector<vector<int>> up;
+ll timer;
+vector<ll> tin, tout;
+vector<vector<ll>> up;
 
-void dfs(int v, int p)
+void dfs(ll v, ll p)
 {
     tin[v] = ++timer;
     up[v][0] = p;
-    for (int i = 1; i <= l; ++i)
-        up[v][i] = up[up[v][i-1]][i-1];
+    for (ll i = 1; i <= l; ++i)
+        up[v][i] = up[up[v][i - 1]][i - 1];
 
-    for (int u : adj[v]) {
+    for (ll u : adj[v]) {
         if (u != p)
             dfs(u, v);
     }
@@ -20,29 +20,29 @@ void dfs(int v, int p)
     tout[v] = ++timer;
 }
 
-bool is_ancestor(int u, int v)
+bool is_ancestor(ll u, ll v)
 {
     return tin[u] <= tin[v] && tout[u] >= tout[v];
 }
 
-int lca(int u, int v)
+ll lca(ll u, ll v)
 {
     if (is_ancestor(u, v))
         return u;
     if (is_ancestor(v, u))
         return v;
-    for (int i = l; i >= 0; --i) {
+    for (ll i = l; i >= 0; --i) {
         if (!is_ancestor(up[u][i], v))
             u = up[u][i];
     }
     return up[u][0];
 }
 
-void preprocess(int root) {
+void preprocess(ll root) {
     tin.resize(n);
     tout.resize(n);
     timer = 0;
     l = ceil(log2(n));
-    up.assign(n, vector<int>(l + 1));
+    up.assign(n, vector<ll>(l + 1));
     dfs(root, root);
 }
