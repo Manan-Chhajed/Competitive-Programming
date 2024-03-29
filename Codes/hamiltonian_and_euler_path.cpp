d58c2fb35fdb40813ab5bf135e70ec5d9750433a
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2024-03-30 00:05:57 +0530

    Update and rename hamiltonian_path.cpp to hamiltonian_and_euler_path.cpp

---
+++ Codes/hamiltonian_and_euler_path.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/contest/1950/problem/G
+// https://codeforces.com/contest/1950/submission/253973481
+// https://www.scaler.com/topics/data-structures/hamiltonian-path/
+
+/* 
+  - Euler's path
+    - Degree of a Vertex: In a graph, the degree of a vertex is defined as the number of edges incident to it.
+    - The theorem states that if a graph has exactly two odd degree vertices, then it has exactly one Euler Path, but no Euler Circuit.
+      Also the Euler Path must start at one of the odd vertices and end at another.
+
+  - Euler's circuit
+    - It states that if all the vertices of a graph component have an even degree (Excluding 0),
+    - then it must have an Euler's Circuit in it and Euler's Circuit can start and end at any vertex.
+*/
--- Codes/hamiltonian_path.cpp
+++
@@ -1,2 +0,0 @@
-// https://codeforces.com/contest/1950/problem/G
-// https://codeforces.com/contest/1950/submission/253973481
